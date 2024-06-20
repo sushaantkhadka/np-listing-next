44dedfdea268c73e506255b2888abf7ec044a51b
Sushant <sushantkhadka2060@gmail.com>
2024-06-20 17:47:26 +0545

    Api call movie

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import ResultCard from "../components/resultCard";
 
-export default function page() {
+const API_KEY = process.env.API_KEY;
+
+
+export default async function page({searchParams}) {
+  const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated'? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  )
+  const data = await res.json();
+  if(!res.ok) {
+    throw new Error ('Failed to fetch Data')
+  }
+  const results = data.results
+  console.log(results)
   return (
-    <div>page</div>
+    <div>
+      <ResultCard  results={results}/>
+    </div>
   )
 }
