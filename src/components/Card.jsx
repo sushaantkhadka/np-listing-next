43da6d6c13aa9b4137a4763065204ecf675bdd59
Sushant <sushantkhadka2060@gmail.com>
2024-06-21 13:20:27 +0545

    margin: card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ export default function Card({ result }) {
   return (
     <div className="group cursor-pointer sm:hover:shadow-slate-400
     sm:shadow-md rounded-lg sm:border
-    sm:border-slate-400 sm:m-2 transition-shadow duration-200 p-2
+    sm:border-slate-400 sm:m-2 transition-shadow duration-200
     ">
       <Link href={`/movie/${result.id}`}>
         <Image
@@ -18,7 +18,7 @@ export default function Card({ result }) {
           height={300}
           className="sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300"
         />
-        <div className="">
+        <div className="m-2">
           <p className="line-clamp-2 text-md text-grey-100 ">
             {result.overview}
           </p>
