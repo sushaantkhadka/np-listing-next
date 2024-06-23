60bdbb1bae415c5a9ee50e2f4c90b8a1583dcd67
Sushant <sushantkhadka2060@gmail.com>
2024-06-23 10:01:59 +0545

    nav change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,8 +5,8 @@ import NavBarItem from "../components/NavBarItem";
 export default function NavBar() {
   return (
     <div className='flex dark:bg-amber-600 bg-grey-600 p-4 lg:text-lg justify-center gap-6'>
-        <NavBarItem title='Trending' param='Trending' />
-        <NavBarItem  title='Top Rated' param='top_rated' />
+        <NavBarItem title='Trending' param='fetchTrending' />
+        <NavBarItem  title='Top Rated' param='fetchTopRated' />
 
 
     </div>
